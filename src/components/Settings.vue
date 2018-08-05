8dc25143a3f37f1618f7a92b3c46dec512b43bfd
Joran <joran.k@gmail.com>
2018-08-05 13:03:18 +1200

    fix settings page

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,35 +1,17 @@
 <template>
   <fieldset class="row">
     <div class="col-sm-12">
-      <p>
-    <h5>Private Identity</h5>
-    <div class="input-group">
-      <span class="input-group-addon" id="identity-key">key</span>
-      <input type="text" class="form-control" v-model.trim="identity.key" placeholder="~/.ssb secret localStorage key" aria-label="~/.ssb secret localStorage key" aria-describedby="identity-key">
-      <span class="input-group-addon" id="identity-value">value</span>
-      <input type="text" class="form-control" v-model.trim="identity.value" @input="updateValue(identity.key, $event.target.value)" placeholder="~/.ssb secret" aria-label="~/.ssb secret" aria-describedby="identity-value">
-    </div>
-          </p>
-      <p>
-    <h5>Websocket URL</h5>
-    <div class="input-group">
-      <span class="input-group-addon" id="remoteWebsocket-key">key</span>
-      <input type="text" class="form-control" v-model.trim="remoteWebsocket.key" placeholder="remote websocket localStorage key" aria-label="remote websocket localStorage key" aria-describedby="identity-key">
-      <span class="input-group-addon" id="remoteWebsocket-value">value</span>
+    
+    <b-form-group label="Secret">
+      <b-form-textarea v-model.trim="identity.value" @input="updateValue(identity.key, $event.target.value)" placeholder="~/.ssb secret">
+      </b-form-textarea>
+    </b-form-group>
+          
+    <b-form-group label="Remote">
       <input type="text" class="form-control" v-model.trim="remoteWebsocket.value" @input="updateValue(remoteWebsocket.key, $event.target.value)" placeholder="websocket url" aria-label="remoteWebsocket-value" aria-describedby="remoteWebsocket-value">
-    </div>
-          </p>
-      <p>
-        A fresh keypair is generated automatically on first load. 
-        If you have an existing scuttleverse identity, you can paste it in above.
-      </p>
-      <p>
-        If you're running sbot locally you will find your keypair in ~.ssb/secret.
-        Please back up your secret securely. Otherwise you could lose that identity.
-      </p>
-      <p>
-        Be careful to never share the private part of the keypair with anyone. They could impersonate you!
-      </p>
+    </b-form-group>
+    
+     
   </div>
     </fieldset>
 </template>
